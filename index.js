cd9fd151dc5213583a51ace361bd76a2255772fd
lola <slonanuga@gmail.com>
2018-11-25 14:53:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much.";
 
-const token = '2cc73f6af4ae1134b3edd61e56ba9c79a1fee057' 
+const token = '' 
 
 fetch('https://api.github.com/repos/jquery/jquery/commits', {headers: {
     Authorization: `token ${token}`}})
